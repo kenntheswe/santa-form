c3ff89bcea09eceea13d4d3b75bc3e9dd4c0c8ee
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-07-20 23:58:09 +0900

    Added client js& fixed user js

--- public/client.js
+++ public/client.js
@@ -1,15 +1,85 @@
 
 // client-side js
 // run by the browser each time your view template is loaded
-
-console.log('hello world :o');
-
-
 // define variables that reference elements on our page
 const santaForm = document.forms[0];
+const wishMessage = document.querySelector("#wish"); 
+const userName = document.querySelector("#username");
+const curChar = document.querySelector("#cur-char");
+
+// form validation 
+const checkInputs = () => {
+  // input values
+  const wishMessageValue = wishMessage.value;
+  const userNameValue = userName.value;
+  const char = wishMessage.value.split("");
+  const charCount = char.length; // count the character by length
+
+  // no character input
+  if (!wishMessageValue) {
+    setError(wishMessage, "Please enter your message.");
+    return false;
+  }
+
+  if (!userNameValue) {
+    setError(userName, "Please enter your username."); // error message for inputting wrong username
+    return false;
+  }
+
+  if (charCount <= 100) {
+    setSuccess(wishMessage); // success message when characters are less than/ equal to 100 characters
+    return true;
+  }
+  setError(wishMessage, "You have input more than 100 characters!"); // error message for inputting more than 100 characters
+  return false;
+}
+
+// error message when no input/ more than 100 characters
+const setError = (input, message) => {
+  const formControl = input.parentElement;
+  const errorMessage = formControl.querySelector("small"); // select error message
+
+  // error message
+  errorMessage.innerText = message;
+
+  // add error to the form class
+  formControl.className = "form-control error";
+}
+
+// success message
+const setSuccess = (input) => {
+  const formControl = input.parentElement;
+
+  // add success to the form class
+  formControl.className = "form-control success";
+}
 
 // listen for the form to be submitted and add a new dream when it is
-santaForm.onsubmit = function (event) {
+santaForm.onsubmit = (event) => {
   // TODO: check the text isn't more than 100chars before submitting
   // event.preventDefault();
+  checkInputs(); // to check user inputs
+
+  if (!checkInputs) {
+    event.preventDefault();
+  }
 };
+
+// listen to the textarea of wish form for character counting
+wishMessage.addEventListener("keyup", () => {
+  const char = wishMessage.value.split("");
+  const charCount = char.length; // count the character by length
+  current.innerText = charCount; // update the character count
+
+  const maxCharLength = santaForm.maxLength; // max character length for wish form
+  const colorChange = .8 * maxCharLength; // color indicator when the character length reaches 80%
+
+  // conditions when textarea occupied by characters up to certain %
+  if (charCount > colorChange && charCount < maxCharLength) {
+    current.style.color = "orange"; // change to orange
+  } else if (charCount >= maxCharLength) {
+    current.style.color = "red"; // change to red
+  } else {
+    current.style.color = "black";
+  }
+});
--- services/user.js
+++ services/user.js
@@ -1,80 +1,66 @@
-// element references on the page
-const wishForm = document.forms[0];
-const wishMessage = document.querySelector("#wish");
-const userName = document.querySelector("#username");
-const curChar = document.querySelector("#cur-char");
+// 
+const axios = require("axios");
 
-// error message when no input/ more than 100 characters
-const setError = (input, message) => {
-  const formControl = input.parentElement;
-  const errorMessage = formControl.querySelector("small"); // select error message
+let userIdMap = new Map();
+let userMap = new Map();
 
-  // error message
-  errorMessage.innerText = message;
+const updateData = () => {
+  userIdMap = new Map();
+  userMap = new Map();
 
-  // add error to the form class
-  formControl.className = "form-control error";
+  // provided json objects
+  const urlOne = "https://raw.githubusercontent.com/alj-devops/santa-data/master/users.json";
+  const urlTwo = "https://raw.githubusercontent.com/alj-devops/santa-data/master/userProfiles.json";
+
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "get",
+      url: urlOne,
+      responseType: "json",
+    })
+    .then((response) => {
+      const userData = response.data;
+
+      // insert data to userIdMap
+      for (data of userData) {
+        userIdMap.set(data.username, data.uid);
+      }
+
+      return axios({
+        method: "get",
+        url: urlTwo,
+        responseType: "json",
+      });
+    })
+    .then((response) => {
+      const userProfile = response.data;
+
+      // insert data to userMap
+      for (data of userProfile) {
+        userMap.set(data.userUid, data)
+      }
+
+      resolve();
+    }).catch((error) => {
+      reject(error);
+    });
+  });
 }
 
-// success message
-const setSuccess = (input) => {
-  const formControl = input.parentElement;
 
-  // add success to the form class
-  formControl.className = "form-control success";
+const getData = (username) => {
+  // username validation
+  if (!username || !userIdMap.has(username)) {
+    return null;
+  }
+
+  const userId = userIdMap.get(username);
+  const birthdate = userMap.get(userId).birthdate;
+  const { address } = userMap.get(userId);
+
+  return {
+    username, userId, birthday, address
+  };
 }
 
-// form validation 
-const checkInputs = () => {
-  // input values
-  const wishMessageValue = wishMessage.value;
-  const userNameValue = userName.value;
-  const char = wishMessage.value.split("");
-  const charCount = char.length; // count the character by length
-
-  // no character input
-  if (!wishMessageValue) {
-    setError(wishMessage, "Please enter your message.");
-    return false;
-  }
-
-  if (!userNameValue) {
-    setError(userName, "Please enter your username."); // error message for inputting wrong username
-    return false;
-  }
-
-  if (charCount <= 100) {
-    setSuccess(wishMessage); // success message when characters are less than/ equal to 100 characters
-    return true;
-  }
-  setError(wishMessage, "You have input more than 100 characters!"); // error message for inputting more than 100 characters
-  return false;
-}
-
-// listen to the form while being submited 
-wishForm.onsubmit = (event) => {
-  checkInputs(); // to check user inputs
-
-  if (!checkInputs) {
-    event.preventDefault();
-  }
-};
-
-// listen to the textarea of wish form for character counting
-wishMessage.addEventListener("keyup", () => {
-  const char = wishMessage.value.split("");
-  const charCount = char.length; // count the character by length
-  current.innerText = charCount; // update the character count
-
-  const maxCharLength = wishForm.maxLength; // max character length for wish form
-  const colorChange = .8 * maxCharLength; // color indicator when the character length reaches 80%
-
-  // conditions when textarea occupied by characters up to certain %
-  if (charCount > colorChange && charCount < maxCharLength) {
-    current.style.color = "orange"; // change to orange
-  } else if (charCount >= maxCharLength) {
-    current.style.color = "red"; // change to red
-  } else {
-    current.style.color = "black";
-  }
-});
+export { updateData, getData};

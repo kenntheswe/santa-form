98fd1a6d7f4d7e2c0650a20031bc0972b7e1dff9
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-07-29 19:21:17 +0900

    Fixed client

--- public/client.js
+++ public/client.js
@@ -7,13 +7,43 @@ const wishMessage = document.querySelector("#wish");
 const userName = document.querySelector("#username");
 const curChar = document.querySelector("#cur-char");
 
+// listen for the form to be submitted and add a new dream when it is
+santaForm.onsubmit = function(event) {
+  // TODO: check the text isn't more than 100chars before submitting
+  // event.preventDefault();
+  checkInputs(); // to check user inputs
+
+  if (!checkInputs()) {
+    event.preventDefault();
+  }
+};
+
+// listen to the textarea of wish form for character counting
+wishMessage.addEventListener("keyup", () => {
+  const charNum = wishMessage.value.split("");
+  const charCount = charNum.length; // count the character by length
+  curChar.innerText = charCount; // update the character count
+
+  const maxCharLength = 100; // max character length for wish form
+  const colorChange = 0.8 * maxCharLength; // color indicator when the character length reaches 80%
+
+  // conditions when textarea occupied by characters up to certain %
+  if (charCount > colorChange && charCount < maxCharLength) {
+    curChar.style.color = "orange"; // change to orange
+  } else if (charCount >= maxCharLength) {
+    curChar.style.color = "red"; // change to red
+  } else {
+    curChar.style.color = "black"; // remain black
+  }
+});
+
 // form validation 
 const checkInputs = () => {
   // input values
   const wishMessageValue = wishMessage.value;
   const userNameValue = userName.value;
-  const char = wishMessage.value.split("");
-  const charCount = char.length; // count the character by length
+  const charNum = wishMessage.value.split("");
+  const charCount = charNum.length; // count the character by length
 
   // no character input
   if (!wishMessageValue) {
@@ -30,7 +60,7 @@ const checkInputs = () => {
     setSuccess(wishMessage); // success message when characters are less than/ equal to 100 characters
     return true;
   }
-  setError(wishMessage, "You have input more than 100 characters!"); // error message for inputting more than 100 characters
+  setError(wishMessage, "You have written more than 100 characters!"); // error message for inputting more than 100 characters
   return false;
 }
 
@@ -53,33 +83,3 @@ const setSuccess = (input) => {
   // add success to the form class
   formControl.className = "form-control success";
 }
-
-// listen for the form to be submitted and add a new dream when it is
-santaForm.onsubmit = (event) => {
-  // TODO: check the text isn't more than 100chars before submitting
-  // event.preventDefault();
-  checkInputs(); // to check user inputs
-
-  if (!checkInputs) {
-    event.preventDefault();
-  }
-};
-
-// listen to the textarea of wish form for character counting
-wishMessage.addEventListener("keyup", () => {
-  const char = wishMessage.value.split("");
-  const charCount = char.length; // count the character by length
-  current.innerText = charCount; // update the character count
-
-  const maxCharLength = santaForm.maxLength; // max character length for wish form
-  const colorChange = .8 * maxCharLength; // color indicator when the character length reaches 80%
-
-  // conditions when textarea occupied by characters up to certain %
-  if (charCount > colorChange && charCount < maxCharLength) {
-    current.style.color = "orange"; // change to orange
-  } else if (charCount >= maxCharLength) {
-    current.style.color = "red"; // change to red
-  } else {
-    current.style.color = "black";
-  }
-});
